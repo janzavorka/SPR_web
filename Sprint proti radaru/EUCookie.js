bb731ff6ab7971c82a7f7ead7e032a06e3584b16
PlechovkaCZ <janzavorka@seznam.cz>
2019-01-11 21:01:21 +0100

    Update web visit analysis

--- Sprint proti radaru/EUCookie.js
+++ Sprint proti radaru/EUCookie.js
@@ -1,5 +1,25 @@
+
+function EUCookieCallback( action, label ) {
+		//console.log(action, label);
+		// Example output: 'no-show', 'unsupported browser'
+		if (label == "consent"){ //If using cookie is OK, analyse
+			console.log("Analýza návštěvnosti povolena");
+			//Google
+			    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			    ga('create', 'UA-102260641-1', 'auto');
+			    ga('send', 'pageview');
+		}
+}
+
 var fucking_eu_config = {
 		"l18n": {
 			"text": "Tento web používá k analýze návštěvnosti soubory cookie. Používáním tohoto webu s tím souhlasíte."
+		},
+		"options": {
+			"callback": EUCookieCallback
 		}
 	};
--- Sprint proti radaru/analyza.js
+++ Sprint proti radaru/analyza.js
@@ -1,4 +1,6 @@
-//Google 
+
+/*
+//Google
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,5 +11,6 @@
 
 //Toplist
     document.write('<a href="https://www.toplist.cz/">');
-    document.write('<img src="https://toplist.cz/dot.asp?id=1773085&amp;http='+escape(document.referrer)+'&amp;t='+escape(document.title)+'&amp;wi='+escape(window.screen.width)+'&amp;he='+escape(window.screen.height)+'&amp;cd='+escape(window.screen.colorDepth)+'" width="1" height="1" border=0 alt="TOPlist" />'); 
+    document.write('<img src="https://toplist.cz/dot.asp?id=1773085&amp;http='+escape(document.referrer)+'&amp;t='+escape(document.title)+'&amp;wi='+escape(window.screen.width)+'&amp;he='+escape(window.screen.height)+'&amp;cd='+escape(window.screen.colorDepth)+'" width="1" height="1" border=0 alt="TOPlist" />');
     document.write('</a>');
+*/
