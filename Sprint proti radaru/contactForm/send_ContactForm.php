e741927e4c47255cb629653c2176801293bab8ba
janzavorka <42653813+PlechovkaCZ@users.noreply.github.com>
2019-01-22 00:04:15 +0100

    Contact form bug fix (issue #9)

--- Sprint proti radaru/contactForm/send_ContactForm.php
+++ Sprint proti radaru/contactForm/send_ContactForm.php
@@ -5,20 +5,27 @@ $url_FAIL =  'http://sprintprotiradaru.cz/kontakty_poodeslanifail.html';
 $mail_stat = FALSE;
 
 
-
-if ((!empty($_POST['email']) or !empty($_POST['text'])) and empty($_POST['url']))  {
+//Form validation (has to be done here if user has JS disabled)
+//Filter for email, message isn't empty and antibot (input which is for human invisible) empty
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) and !empty($_POST['text']) and empty($_POST['url']))  {
+  //Read inputs
   $mail = test_input($_POST['email']);
   $message = test_input($_POST['text']);
+  //Prepare email
 	$to = "info@sprintprotiradaru.cz";
 	$extra .= "From: ".$mail."\r\nReply-To: ".$mail."\r\n";
   $extra .= "MIME-Version: 1.0\n";
-  $extra .= "Content-Type: text/plain; charset=\"utf-8\"\n";
-  $extra .= "Content-Transfer-Encoding: base64\n";
 	$subject = "Dotaz od ".$mail."";
 	$mess ="Email: ".$mail."
 		\nText:\n".$message."";
+
   //Send email
-  $mail_stat =  mail ($to, $subject, $mess, $extra);
+  mb_language('Neutral');
+  mb_internal_encoding("UTF-8");
+  mb_http_input("UTF-8");
+  mb_http_output("UTF-8");
+
+  $mail_stat =  mb_send_mail ($to, $subject, $mess, $extra);
   if($mail_stat){
     header('Location: '.$url_OK, true, $code);
   }
